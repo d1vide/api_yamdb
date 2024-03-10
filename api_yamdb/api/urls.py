553a7e07690a8e001cc4c6361ff5bfd193c91e84
Гаджимурад Саидов <no884990@gmail.com>
2024-03-10 22:19:37 +0300

    add url Reviews in urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from .views import ReviewViewSet
 from api.views import CategoryViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register(r'categories', CategoryViewSet)
-
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
