edf44ca22a08e77203eddbbafc8fb6515cb4bb79
Гаджимурад Саидов <no884990@gmail.com>
2024-03-10 08:13:56 +0300

    Update models and add permissons

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,10 +1,57 @@
 from rest_framework import serializers
 
 from reviews.models import Category
-
+from django.shortcuts import get_object_or_404
+from reviews.models import Comment, Review
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         exclude = ('id',)
         model = Category
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    title = serializers.SlugRelatedField(
+        slug_field='id',
+        many=False,
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def validate(self, data):
+        if self.context['request'].method != 'POST':
+            return data
+        title = get_object_or_404(
+            Title, pk=self.context['view'].kwargs.get('title_id')
+        )
+        author = self.context['request'].user
+        if Review.objects.filter(title_id=title, author=author).exists():
+            raise serializers.ValidationError(
+                'Вы уже оставили отзыв!'
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        many=False,
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
