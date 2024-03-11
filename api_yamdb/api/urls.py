2a8c6c06426f5ef9371464b933d63a7ef6121121
Алексей Наумов <aleksey_naumov_03@list.ru>
2024-03-11 19:38:14 +0300

    ReviewViewSet зарегестрирован

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       TitleViewSet)
+                       ReviewViewSet, TitleViewSet)
 
 router_v1 = DefaultRouter()
 
@@ -13,6 +13,12 @@ router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
